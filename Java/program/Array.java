1f42c7e8aa23a71f08e72e0c1c71af604d276995
Rishabh Shrivastava <rishabhshrivastava7565@gmail.com>
2025-09-09 15:49:36 +0530

    new

---
+++ Java/program/Array.java
@@ -0,0 +1,10 @@
+package Java.program;
+
+public class Array {
+    public static void main(String[] args) {
+        int[] numbers = {10, 20, 30, 40, 50};
+        for (int n : numbers) {
+            System.out.println(n);
+        }
+    }
+}
